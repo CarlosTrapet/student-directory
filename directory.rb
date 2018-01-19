a7304aac9afc04281cfcdc1bbec7186b32e25d95
carlostrapet <trapet_llamas@hotmail.com>
2018-01-19 12:44:30 +0000

    refactored, added hash

--- directory.rb
+++ directory.rb
@@ -29,7 +29,22 @@ def print_footer(names)
 puts "Overwall, we have #{names.count} villains enrolled in our Villains Academy"
 end
 
-# execute methods
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
