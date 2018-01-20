cad7f38f153de76322541eed0c02e6077e918aae
carlostrapet <trapet_llamas@hotmail.com>
2018-01-20 23:19:56 +0000

    added create file

--- directory.rb
+++ directory.rb
@@ -9,11 +9,11 @@ def print_header
 end
 
 def print_footer
-  if @students.count > 1 
-    puts "Overall, we have #{@students.count} villains enrolled in our Villains Academy"
+  if @students.count == 1 
+    puts "Overall, we have #{@students.count} villain enrolled in our Villains Academy"
     puts "--------------"
   else 
-    puts "Overall, we have #{@students.count} villain enrolled in our Villains Academy"
+    puts "Overall, we have #{@students.count} villains enrolled in our Villains Academy"
     puts "--------------"
   end
 end
@@ -47,6 +47,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the students to a file"
     puts "9. Exit"
 end
 
@@ -62,6 +63,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -74,6 +77,16 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
